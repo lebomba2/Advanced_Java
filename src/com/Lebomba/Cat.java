9b74ee5802b48f62fe56090109e780f40b003cbb
lebomba2 <lebomba2@gmail.com>
2018-01-27 12:55:00 -0600

    Added Animals Files

---
+++ src/com/Lebomba/Cat.java
@@ -0,0 +1,39 @@
+package com.Lebomba;
+
+/**
+ * Created by mgreen14 on 12/27/17.
+ * @author Chris LeBombard
+ *
+ */
+public class Cat extends Pet implements Talkable {
+    private int mousesKilled;
+
+    /**
+     *
+     * @param mousesKilled
+     * @param name
+     */
+    public Cat(int mousesKilled, String name) {
+        super(name);
+        this.mousesKilled = mousesKilled;
+    }
+
+    public int getMousesKilled() {
+        return mousesKilled;
+    }
+
+    public void addMouse() {
+        mousesKilled++;
+    }
+
+    @Override
+    public String talk() {
+        return "Meow";
+    }
+
+    @Override
+    public String toString() {
+        return "Cat: " + "name=" + name + " mousesKilled=" + mousesKilled;
+    }
+}
+
